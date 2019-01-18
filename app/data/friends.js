671b2eef3e113717d15b3ddc40e7a803f0deaeb8
Mwilson7992 <Mwilson091416@gmail.com>
2019-01-18 13:23:50 -0600

    work work work

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendData = [
+	{
+		name: "Michael Scott",
+		photo: '<img src="images/scott.jpg" border="0" alt="Michael Scott">',
+		scores: [4,3,2,5,5,2,3,1,5,4]
+	},
+     {
+		name: "Jim Halpert",
+		photo: '<img src="images/jim.jpg" border="0" alt="Jim Halpert">',
+		scores: [1,5,4,3,2,5,5,3,5,2]
+	},
+     {
+		name: "Dwight Schrute",
+		photo: '<img src="images/dwight.jpg" border="0" alt="Dwight Schrute">',
+		scores: [5,1,2,1,3,2,2,5,1,5]
+	}
+];
+
+module.exports = friendData;
